086d9568f026f65be9260d5406eb10ea4afed01a
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2022-09-17 15:46:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-function preload(){
-
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', { probabilityThreshold: 0.7 } ,modelReady);
 }
 
-function setup() {
-canvas= createCanvas(600,500);
-canvas.position(100,250);
-video=createCapture(VIDEO);
-video.hide();
-tint_color="";
+function modelReady(){
+  classifier.classify(gotResults);
 }
 
-function draw() {
-    image(video,100,80,300,250   );
+var dog = 0;
+var cat = 0;
 
-    circle(40,40,50);
-    circle(460,40,50);
-    circle(40,360,50);
-    circle(460,360,50);
-
-    rect(70, 27, 375, 25);
-    rect(70, 350, 375, 25);
-    rect(30, 63, 25, 275);
-    rect(448, 63, 25, 275);
-
-     
-}
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
 
 
-function take_snapshot() {
-    save("");
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+      img.src = 'bark.gif';
+      dog = dog+1;
+    } else if (results[0].label == "Meowing") {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen.gif';
     }
-
+  }
+}
