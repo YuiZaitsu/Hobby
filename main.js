bc4785e9e416ebebf68ca408b2726cae828ca2e8
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2023-01-28 22:34:12 +0530

    Add files via upload

--- main.js
+++
@@ -1,8 +0,0 @@
-quick_draw_data_set=["[‘pen’,’paper’,’book’,’bottle’]"]
-Element_of_array = quick_draw_data_set[random_no];
-document.getElementById("sketch_to_be_drawn").innerHTML = "Sketch To Be Drawn: "+Element_of_array;
-timer_counter = 0;
-timer_check = "";
-drawn_sketch = "";
-answer_holder = "";
-score = 0;
